6cd46c8cfe026f250ca3ea021ae53735644a8222
Kaludjerovic <koljakalud@gmail.com>
2023-05-13 10:15:01 +0200

    Two threads

---
+++ ThreadingLesson/ThreadingLesson/NumberPrinter.cs
@@ -0,0 +1,21 @@
+﻿namespace ThreadingLesson;
+
+public class NumberPrinter
+{
+    private int _number = 0;
+    private readonly string _instanceName;
+    private readonly Random _random = new Random();
+
+    public NumberPrinter(string instanceName)
+    {
+        _instanceName = instanceName;
+    }
+    
+    public void PrintNumber()
+    {
+        Console.WriteLine($"{ _instanceName }: { _number }");
+        _number++;
+        
+        Thread.Sleep(_random.Next(100, 5000));
+    }
+}
---
+++ ThreadingLesson/ThreadingLesson/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ThreadingLesson;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var thread1 = new Thread(StartPrinter1);
+        var thread2 = new Thread(StartPrinter2);
+        
+        thread1.Start();
+        thread2.Start();
+
+        Console.ReadLine();
+    }
+
+    public static void StartPrinter1()
+    {
+        var printer1 = new NumberPrinter("Printer 1");
+        
+        while (true)
+        {
+            printer1.PrintNumber();
+        }
+    }
+    
+    public static void StartPrinter2()
+    {
+        var printer2 = new NumberPrinter("Printer 2");
+        
+        while (true)
+        {
+            printer2.PrintNumber();
+        }
+    }
+}
+
+
+
